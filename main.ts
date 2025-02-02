cf4a5e624a50801a30a9ae8ac497afe6eb8c3c32
Grol Grol <grol666@gmail.com>
2025-02-03 02:02:13 +0300

    fix custom checkbox and cursor

--- main.ts
+++ main.ts
@@ -11,11 +11,12 @@ export default class CheckboxSyncPlugin extends Plugin {
   updateParentCheckboxes(editor: Editor) {
     const lines = editor.getValue().split("\n");
     let hasChanges = false;
+    const cursor = editor.getCursor();
 
     for (let i = lines.length - 1; i >= 0; i--) {
-      const match = lines[i].match(/^(\s*)- \[([ x])\] (.*)$/);
+      const match = lines[i].match(/^(\s*)- \[(.)\] (.*)$/);
       if (!match) continue;
-
+      
       const indent = match[1].length;
       const isChecked = match[2] === "x";
       let allChildrenChecked = true;
@@ -23,7 +24,7 @@ export default class CheckboxSyncPlugin extends Plugin {
       let j = i + 1;
 
       while (j < lines.length) {
-        const childMatch = lines[j].match(/^(\s*)- \[([ x])\] (.*)$/);
+        const childMatch = lines[j].match(/^(\s*)- \[(.)\] (.*)$/);
         if (!childMatch || childMatch[1].length <= indent) break;
         hasChildren = true;
         if (childMatch[2] !== "x") allChildrenChecked = false;
@@ -43,6 +44,7 @@ export default class CheckboxSyncPlugin extends Plugin {
 
     if (hasChanges) {
       editor.replaceRange(lines.join("\n"), { line: 0, ch: 0 }, { line: editor.lastLine(), ch: editor.getLine(editor.lastLine()).length });
+      editor.setCursor(cursor);
     }
   }
 }
